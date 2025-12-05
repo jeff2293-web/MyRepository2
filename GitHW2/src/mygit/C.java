0a417a106b2f7795c417c8b3d9eda4f7bf3a0bd7
김건우 <@192.168.219.101>
2025-12-06 00:28:10 +0900

    Change master branch

--- GitHW2/src/mygit/C.java
+++ GitHW2/src/mygit/C.java
@@ -5,6 +5,7 @@ public class C {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("How can I get to Gileum Station?");
+		System.out.println("Where is the bus stop?");
 	}
 
 }
