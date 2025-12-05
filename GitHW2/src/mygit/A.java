6877b7453804162561ec296839d2fb463958591a
김건우 <@192.168.219.101>
2025-12-06 00:21:09 +0900

    commit Alice branch

--- GitHW2/src/mygit/A.java
+++ GitHW2/src/mygit/A.java
@@ -6,6 +6,7 @@ public class A {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, I'm Geonu!");
 		System.out.println("I'm 23 years old");
+		System.out.println("Nice to meet you");
 	}
 
 }
