301c18083f7d81e1458be943434f3d5caf736360
김건우 <@192.168.219.101>
2025-12-06 00:42:11 +0900

    Change C.java in Bob branch

--- GitHW2/src/mygit/C.java
+++ GitHW2/src/mygit/C.java
@@ -6,6 +6,7 @@ public class C {
 		// TODO Auto-generated method stub
 		System.out.println("How can I get to Gileum Station?");
 		System.out.println("Where is the bus stop?");
+		System.out.println("Thank you.");
 	}
 
 }
