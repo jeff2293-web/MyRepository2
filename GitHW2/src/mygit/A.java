d42d8f16fd0d001d138bdd1cd3185e134b8a7d39
김건우 <@192.168.219.101>
2025-12-05 23:55:53 +0900

    add a println line

--- GitHW2/src/mygit/A.java
+++ GitHW2/src/mygit/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, I'm Geonu!");
+		System.out.println("I'm 23 years old");
 	}
 
 }
